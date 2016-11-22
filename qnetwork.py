1adc1ff3c3d466ed299cf9127cb3446d9e6127d4
Brian Beallo <bbeallo12@yahoo.com>
2016-11-21 20:37:57 -0800

    improved reward matrix

--- qnetwork.py
+++ qnetwork.py
@@ -20,7 +20,6 @@ import scipy.sparse as sps
 # stateet the next state astate the current state.
 # End Do
 # End For
-
 class Part(object):
     'component object'
     def __init__(self, partType, infoID=0, partNum=0, mft=0, mrt=0, label=0, state=['0'], KofN=1, reliability = 0):
@@ -64,7 +63,7 @@ class Part(object):
 
 class QNetwork(object): # qnetwork class
     'Qnetwork class'
-    def __init__(self, alpha=0.5, gamma=0.9): # constructor, alpha = learn rate, gamma = memory
+    def __init__(self, alpha=0.9, gamma=1.0): # constructor, alpha = learn rate, gamma = memory
         self.alpha = alpha # set alpha
         self.gamma = gamma # set gamma
         self.count = 1 # initialize to something, I just chose 1
@@ -78,8 +77,9 @@ class QNetwork(object): # qnetwork class
         self.q_value = sps.lil_matrix(reward.shape) # set Q value matrix
         self.state = state # set initial state
 
-    def train(self, cycles):
+    def train(self, cycles, mrts, maxtime, maxRel):
         n = 0
+        time = 0
         while cycles[0]:#training cycles
             n += 1
             state = self.state #set current state
@@ -88,12 +88,25 @@ class QNetwork(object): # qnetwork class
                 actions = np.argsort(self.q_value[state, :].toarray(),kind='mergesort')[0][::-1]
                 action = actions[self.reward[state, actions].toarray(0)[0] != 0][0]
                 # increment Q value  -  multiline
-                self.q_value[state, action] = self.q_value[state, action] + \
-                self.alpha*(self.reward[state, action] + \
-                self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
-                self.q_value[state, action])
-                if state == action: # test if next state is the same as the current state
-                    action = ran.randint(0, self.count-1)
+                if state != action: # test if next state is the same as the current state
+                    self.q_value[state, action] = self.q_value[state, action] + \
+                    self.alpha*(self.reward[state, action] + \
+                    self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+                    self.q_value[state, action])
+                    part = int(math.log(action ^ state, 2))
+                    if time < maxtime:
+                        time += mrts[part]
+                else:
+                    self.q_value[state, action] = self.q_value[state, action] + \
+                    self.alpha*(self.reward[state, action] + maxRel*(maxtime - time)  + \
+                    self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+                    self.q_value[state, action])
+                    r = ran.randint(1, 100) #get random number from 1-100
+                    if r > 10:
+                        action = ran.randint(0, self.count-1)
+                    else:
+                        action = 0
+                    time = 0
                 self.state = action# set next state
             else: # 30% of the time
                 indices = np.nonzero(self.reward[state, :].toarray()[0])[0]
@@ -103,42 +116,76 @@ class QNetwork(object): # qnetwork class
                 action = ran.randint(0, len(indices)-1) # get random action
                 action = indices[action]
                 # increment Q value
-                self.q_value[state, action] = self.q_value[state, action] + \
-                self.alpha*(self.reward[state, action] + \
-                self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
-                self.q_value[state, action])
-                if state == action: # test if next state is the same as the current state
-                    action = ran.randint(0, self.count-1)
+                if state != action: # test if next state is the same as the current state
+                    self.q_value[state, action] = self.q_value[state, action] + \
+                    self.alpha*(self.reward[state, action] + \
+                    self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+                    self.q_value[state, action])
+                    part = int(math.log(action ^ state, 2))
+                    if time < maxtime:
+                        time += mrts[part]
+                else:
+                    self.q_value[state, action] = self.q_value[state, action] + \
+                    self.alpha*(self.reward[state, action] + maxRel*(maxtime - time)  + \
+                    self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+                    self.q_value[state, action])
+                    r = ran.randint(1, 100) #get random number from 1-100
+                    if r > 10:
+                        action = ran.randint(0, self.count-1)
+                    else:
+                        action = 0
+                    time = 0
                 # set next state
                 self.state = action
         return
-    def search(self, state=0): # get fix path from state
+    def search(self, maxtime, mrts, maxRel, state=0): # get fix path from state
+        time = 0
         path = [] # component path
         states = [state] # state path
-        cost = 0 # initial cost
-        limit = int(math.log(self.count)/math.log(2))
+        #cost = 0 # initial cost
+        limit = int(math.log(self.count, 2))
         for i in range(limit+1): # loop up to number of states
             # get the sorted indices of the row of the q matrix for the current state and reverse it
             test1 = self.q_value[state, :].toarray()[0]
             test2 = self.reward[state, :].toarray()[0]
             actions = np.argsort(self.q_value[state, :].toarray(),kind='mergesort')[0][::-1]
             action = actions[self.reward[state, actions].toarray(0)[0] != 0][0]
-            # iterate through actions
-            # increment Q value
-            self.q_value[state, action] = self.q_value[state, action] + \
-            self.alpha*(self.reward[state, action] + \
-            self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
-            self.q_value[state, action])
-            cost += self.reward[state, action] # add to cost
-            if state == action:# if next state is the same as current state found end of path
-                return [path, states] # return path and cost
+
+            if state == action:
+                return [path, states]
+            part = int(math.log(action ^ state, 2))
+            path.append(part)
+            states.append(action)
+            # if next state is the same as current state found end of path
+            # return path and cost
             # from state and action
             # calcuate repaired part ID
-            temp = int(math.log(action ^ state)/math.log(2)) 
-            path.append(temp) # append part ID
-            states.append(action)
-
             state = action # set next action
+
+            # iterate through actions
+            # increment Q value
+            #self.q_value[state, action] = self.q_value[state, action] + \
+            #self.alpha*(self.reward[state, action] + \
+            #self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+            #self.q_value[state, action])
+            #cost += self.reward[state, action] # add to cost
+ #           if state != action: # test if next state is the same as the current state
+ #               self.q_value[state, action] = self.q_value[state, action] + \
+ #               self.alpha*(self.reward[state, action] + \
+ #               self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+ #               self.q_value[state, action])
+ #               part = int(math.log(action ^ state, 2))
+ #               path.append(part)
+ #               states.append(action)
+ #               if time < maxtime:
+ #                   time += mrts[part]
+ #           else:
+ #               self.q_value[state, action] = self.q_value[state, action] + \
+ #               self.alpha*(self.reward[state, action] + maxRel*(maxtime - time)  + \
+ #               self.gamma*np.max(self.q_value[action, :].toarray()[0]) - \
+ #               self.q_value[state, action])
+ #               return [path, states]
+            
         return None # if no path found return None
     #print q matrix
     def fromStr(self, string):
@@ -199,13 +246,14 @@ class maintenance(object): # main class
         self.currentState = ['0']
         self.structureMap = []
         self.train = [True]
-        self.fixTime = None
+        self.fixTime = 0
         self.maxTime = None
         self.states = None
         self.rewards = None
         self.qnetwork = None
         self.q_thread = None
         self.file = None
+        self.mrts = None
     def saveQ(self, filename):
         self.train[0] = False
         self.q_thread.join()
@@ -213,7 +261,7 @@ class maintenance(object): # main class
         self.file.write(self.qnetwork.toStr())
         self.file.close()
         self.train[0] = True
-        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train,))
+        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train, self.mrts, self.fixTime, self.states[-1], ))
         self.q_thread.start()
     def loadQ(self, filename):
         self.train[0] = False
@@ -223,7 +271,7 @@ class maintenance(object): # main class
         self.qnetwork.fromStr(string)
         self.file.close()
         self.train[0] = True
-        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train,))
+        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train, self.mrts, self.fixTime, self.states[-1], ))
         self.q_thread.start()
     def setStructure(self, Filename): # get structure file to parse
         root = ElementTree.parse(Filename) # parse xml file
@@ -231,20 +279,30 @@ class maintenance(object): # main class
         structure = root.find('structure/part') # get structure
         parts = root.findall("structure//*[@type='part']") # get all structure parts
         self.stateCount = len(parts) # number of parts in structure
+
         # not including series and parallel groupings
         self.__genPartList(components, self.currentState) # generate part list
+        self.fixTime = 0
+        for part in parts:
+            i = int(part.attrib['infoID'])
+            self.fixTime += self.parts[i].mrt
+        self.__genRel()
         self.Tree = self.__genTree(structure) # generate part tree
-        #self.__genStructureMap(parts) # generate structure mapping array
+        self.mrts = []
+        for i in range(self.stateCount):
+            self.mrts.append( self.parts[self.structureMap[i]].mrt )
         self.states = [0 for j in range(2**self.stateCount)] # initialize reliability array
         for i in range(2**self.stateCount): # increment through all states
             #self.currentState[0] = bin(i)[2:][::-1] # convert state to binary
             self.currentState[0] = bin(i)[2:][::-1] # convert state to binary
+            if i == 63:
+                print ''
             self.states[i] = self.__calcStates(self.Tree) # calculate reliability for current state
             #print(self.currentState[0],self.states[i])
         self.__genRewards() # generate reward matrix
         self.qnetwork = QNetwork()
         self.qnetwork.init_network(self.rewards)
-        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train,))
+        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train, self.mrts, self.fixTime, self.states[-1], ))
         self.q_thread.start()
 
     def convert2Label(self, A): # convert part ID to part Label
@@ -277,9 +335,9 @@ class maintenance(object): # main class
         for I in IDs:
             broken[I] = '0'
         state = int('0b'+''.join(broken[::-1]),2)
-        fix = self.qnetwork.search(state)
+        fix = self.qnetwork.search(self.fixTime, self.mrts, self.states[-1], state)
         self.train[0] = True
-        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train,))
+        self.q_thread = threading.Thread(target=self.qnetwork.train,args=(self.train, self.mrts, self.fixTime, self.states[-1], ))
         self.q_thread.start()
         return fix
     def evaluate(self, fix):
@@ -289,12 +347,13 @@ class maintenance(object): # main class
         total_time = 0
         for i in range(len(parts)):
             reliab = self.states[states[i]]
-            reliab2 = self.states[states[i+1]]
+            #reliab2 = self.states[states[i+1]]
             time = self.parts[self.structureMap[parts[i]]].mrt
-            score = time * (reliab + reliab2) * 0.5
+            score = time * reliab
             total_time += time
             total_score += score
-        return total_score/total_time
+        remain_score = (self.fixTime - total_time) * self.states[-1]
+        return (total_score + remain_score)/self.fixTime
         print 'test'
     def printParts(self):
         out1 = ""
@@ -317,10 +376,10 @@ class maintenance(object): # main class
                     rows.append(i)
                     cols.append(temp)
                     CONST = 1
-                    val = CONST*(self.states[temp] - self.states[i]) - self.parts[self.structureMap[j]].mrt/self.maxTime
-                    #val = 0.5*(self.states[temp] + self.states[i]) * self.parts[self.structureMap[j]].mrt/self.fixTime
-                    #if val == 0:
-                    #    val = 0.001
+                    #val = CONST*(self.states[temp] - self.states[i]) - self.parts[self.structureMap[j]].mrt/self.maxTime
+                    val = self.states[i] * self.parts[self.structureMap[j]].mrt/self.fixTime
+                    if val == 0:
+                        val = -0.1
                     if maxim == None or val > maxim:
                         maxim = val
                     data.append(val)
@@ -357,7 +416,6 @@ class maintenance(object): # main class
         partCount = len(components)
         self.partcount = partCount
         partlist = []
-        self.fixTime = 0
         self.maxTime = 0
         #def __init__(self, partType, infoID=0, mft=0, mrt=0, label=0, state=['0'], KofN=1):
         for part in components:
@@ -369,10 +427,10 @@ class maintenance(object): # main class
                                  state=state))
             if float(part.attrib['mrt']) > self.maxTime:
                 self.maxTime = float(part.attrib['mrt'])
-            self.fixTime += float(part.attrib['mrt'])
-        for part in partlist:
-            part.reliability = math.exp(-self.fixTime/part.mft)
         self.parts = partlist
+    def __genRel(self):
+        for part in self.parts:
+            part.reliability = math.exp(-self.fixTime/part.mft)
 
 
     def __calcStates(self, part): # calculate state reliabilities 
